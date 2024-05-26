e5090a6a3041d3370aead58a3867fc3f8dc84acf
alexi.courieux <courieuxa@gmail.com>
2024-05-26 17:41:21 -0400

    Level ending

---
+++ Assets/Scripts/LevelEndingTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace CoolDawn
+{
+    public class LevelEndingTrigger : MonoBehaviour
+    {
+        [SerializeField] private Loader.Scene nextScene;
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            Loader.Load(nextScene);
+        }
+    }
+}
